a0cc92900954dae5fe90f1ef7e799a47a2761f72
cschaf8 <cschaf8@uic.edu>
2022-01-26 13:34:58 -0600

    circles change color and size accordingly

--- project.js
+++ project.js
@@ -37,20 +37,23 @@ function setup() {
     y = windowHeight/255;
     var1 = map(mouseX/x, 0, 255, 0, 255);
     var2 = map(mouseY/y, 0, 255, 0, 255);
+    radius1 = (windowWidth/mouseX)*30;
+    radius3 = (windowWidth/(windowWidth-mouseX))*30;
+    radius2 = abs(radius1-radius3)/2;
 
     strokeWeight(0);
+
     fill(var1,var2,220);
-    circle(windowWidth/7,windowHeight/7, windowWidth/12);
+    circle(windowWidth/6,windowHeight/6, radius1);
+    circle(windowWidth/6,5*windowHeight/6, radius1);
+
     fill(var1-(255-var1), var2-(255-var2),220);
-    circle(windowWidth/2,windowHeight/7, windowWidth/12);
+    circle(windowWidth/2,windowHeight/6, radius2);
+    circle(windowWidth/2,5*windowHeight/6, radius2);
+
     fill(255-var1,255-var2,220);
-    circle(6*windowWidth/7,windowHeight/7, windowWidth/12);
-    fill(var1,var2,220);
-    circle(windowWidth/7,6*windowHeight/7, windowWidth/12);
-    fill(var1-(255-var1), var2-(255-var2),220);
-    circle(windowWidth/2,6*windowHeight/7, windowWidth/12);
-    fill(255-var1,255-var2,220);
-    circle(6*windowWidth/7,6*windowHeight/7, windowWidth/12);
+    circle(5*windowWidth/6,windowHeight/6, radius3);
+    circle(5*windowWidth/6,5*windowHeight/6, radius3);
 
 
   }
