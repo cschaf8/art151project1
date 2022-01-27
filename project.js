933bc82a4dab5119a722afea1213d6bb7254cb88
cschaf8 <cschaf8@uic.edu>
2022-01-26 21:50:21 -0600

    background changes

--- project.js
+++ project.js
@@ -10,8 +10,23 @@ function setup() {
   list1 = [25,75,125];
   list2 = [20,120,240];
 
+  // colorR = (255,204,204);
+  // colorO = (255,229,204);
+  // colorY = (255,255,204);
+  // colorG = (204,255,204);
+  // colorB = (204,255,255);
+  // colorI = (229,204,255);
+  // colorV = (255,204,255);
+  // colorW = (255,255,255);
+  // colorBl = (0, 0, 0);
+
+  i = 0;
+
+  // roygbiv = [colorR, colorO, colorY, colorG, colorB, colorI, colorV, colorW, colorBl];
+  roygbiv = ['#FFCCCC', '#FFE5CC', '#FFFFCC', '#CCFFCC', '#CCFFFF', '#E5CCFF', '#FFCCFF', '#FFFFFF', '#000000'];
+
   function draw() {
-    background(240);
+    background(roygbiv[i]);
     /*
     //fill(112,155,220);
     strokeWeight(0);
@@ -51,34 +66,45 @@ function setup() {
     }
     
     strokeWeight(0);
-    col1 = color(var1,var2,220);
-    col2 = color(255-var1,255-var2,220);
+    col1 = color(var1,var2,255);
+    col2 = color(255-var1,255-var2,255);
 
     //fill(var1,var2,220);
-    fill(var1,0,var2);
+    //fill(var1,0,var2);
+    fill(var1,var2,255);
     circle(windowWidth/6,windowHeight/6, radius1);
     circle(windowWidth/6,5*windowHeight/6, radius1);
-    fill(255-var1,0,255-var2)
+    //fill(255-var1,0,255-var2)
+    fill(255-var1,255-var2,255);
     circle(windowWidth/6,windowHeight/6, radius1/2);
     circle(windowWidth/6,5*windowHeight/6, radius1/2);
 
     //fill(var1-(255-var1), var2-(255-var2),220);
-    inter = lerpColor(col1,col2,0.25);
-    inter2 = lerpColor(col1,col2,0.75);
+    inter = lerpColor(col1,col2,0.5);
     fill(inter);
     circle(windowWidth/2,windowHeight/6, radius2);
     circle(windowWidth/2,5*windowHeight/6, radius2);
-    fill(inter2);
-    circle(windowWidth/2,windowHeight/6, radius2/2);
-    circle(windowWidth/2,5*windowHeight/6, radius2/2);
 
     //fill(255-var1,255-var2,220);
-    fill(255-var1,0,255-var2);
+    //fill(255-var1,0,255-var2);
+    fill(255-var1,255-var2,255);
     circle(5*windowWidth/6,windowHeight/6, radius3);
     circle(5*windowWidth/6,5*windowHeight/6, radius3);
-    fill(var1,0,var2);
+    //fill(var1,0,var2);
+    fill(var1,var2,255);
     circle(5*windowWidth/6,windowHeight/6, radius3/2);
     circle(5*windowWidth/6,5*windowHeight/6, radius3/2);
 
 
+  }
+
+  function mousePressed() {
+    console.log(i);
+    console.log(roygbiv[i]);
+    i++;
+
+    if(i >= 9)
+    {
+      i = 0;
+    }
   }
